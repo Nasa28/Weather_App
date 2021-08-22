da04832e31fee9823135db6324c373f6ea1764e7
Nasa28 <kaluchinasaagu@gmail.com>
2021-08-22 19:37:02 +0100

    get input value

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 import './assets/css/styles.css'
 import 'bootstrap'
+const search = document.querySelector('.search')
+const city= document.querySelector('.search-input')
+const temp = document.querySelector('.temp')
+const  humidity = document.querySelector('.humidity')
+const wind = document.querySelector('.wind')
+const desc = document.querySelector('.desc')
+const cityName = document.querySelector('.city')
+const icons = document.querySelector('.icon')
 
 async function fetchData(){
-  const response = await fetch()
-    const result = await response.json();
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=&units=metric`;
 
-    const city = result.name
-    const temp = result.main.temp
-    const description = result.weather[0].description
-    const humidity = result.main.humidity
-    const wind = result.wind.speed
-    console.log(city, temp, description, humidity, wind);
+   const response = await fetch(url, {mode: 'cors'})
+    const data = await response.json();
+    const cityValue = data.name
+    const tempValue = data.main.temp
+    const descValue = data.weather[0].description
+    const humidityValue = data.main.humidity
+    const windValue= data.wind.speed
+    const iconValue = data.weather[0].icon
+    
+    cityName.innerHTML = `${cityValue}`
+    temp.innerHTML = `${tempValue} °C`
+    desc.innerHTML =  descValue.toUpperCase()
+    humidity.innerHTML = `HUMIDITY: ${humidityValue}%`
+    wind.innerHTML = `WIND SPEED: ${windValue}mph`
+    icons.src =`https://openweathermap.org/img/wn/${iconValue}.png`
+
 }
-    fetchData()
+
+search.addEventListener('click', ()=>{
+  fetchData()
+})
