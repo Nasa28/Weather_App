76677d4a099e3aee672b79f5058be3cd3e94bf72
Nasa28 <kaluchinasaagu@gmail.com>
2021-08-24 00:51:28 +0100

    fix linters

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 import * as dom from './dom';
 
-export default async function fetchData() {
+export default  fetchData = async ()=> {
   const apikey = 'bc92c530991c288d75d5cf2407507597';
   const location = dom.city.value || 'enugu';
   const url = `
