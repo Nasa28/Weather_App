e316850f7059338996e7588ae28b63a578ae0cef
Nasa28 <kaluchinasaagu@gmail.com>
2021-08-23 12:19:12 +0100

    add converter function

--- src/data.js
+++ src/data.js
@@ -1,22 +1,45 @@
 import * as dom from './dom';
 
 export default async function fetchData(){
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${dom.city.value || "enugu"}&appid=&units=metric`;
+  
+  const location= dom.city.value ||'enugu'
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}&units=metric`;
 
-   const response = await fetch(url, {mode: 'cors'})
+ try{ const response = await fetch(url, {mode: 'cors'})
     const data = await response.json();
     const cityValue = data.name
-    const tempValue = data.main.temp
+    const tempValue = data.main.temp.toFixed(0)
     const descValue = data.weather[0].description
     const humidityValue = data.main.humidity
     const windValue= data.wind.speed
     const iconValue = data.weather[0].icon
-    
-    dom.cityName.innerHTML = `${cityValue}`
-    dom.temp.innerHTML = `${tempValue} °C`
-    dom.desc.innerHTML =  descValue.toUpperCase()
-    dom.humidity.innerHTML = `HUMIDITY: ${humidityValue}%`
-    dom.wind.innerHTML = `WIND SPEED: ${windValue}mph`
+    dom.cityName.textContent = `${cityValue}`
+    dom.temp.textContent = `${tempValue} °C`
+    dom.desc.textContent  =  descValue.toUpperCase()
+    dom.humidity.textContent  = `HUMIDITY: ${humidityValue}%`
+    dom.wind.textContent  = `WIND SPEED: ${windValue}mph`
     dom.icons.src =`https://openweathermap.org/img/wn/${iconValue}.png`
-  document.querySelector('.weather').classList.remove("loading")
+    document.querySelector('.weather').classList.remove("loading");
+
+    
+    dom.convert.addEventListener("click", ()=>{
+      if(dom.temp.textContent.includes("°F")){
+        dom.temp.textContent = `${tempValue} °C`
+      }
+    })
+
+    dom.converter.addEventListener("click", ()=>{
+      if( dom.temp.textContent.includes("°C")) 
+      dom.temp.textContent = `${Math.floor(tempValue *1.8 + 32)} °F`              
+    })
+
+  }catch (error){
+    if(error){
+      dom.errors.innerHTML = `${dom.city.value.toUpperCase()} is not a valid City name`
+      setTimeout("location.reload(true);",1500);
+    }
+  }
+  
 }
+
+
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import './assets/css/styles.css'
 import 'bootstrap'
-import fetchData from './data'
 import * as dom from './dom';
+import fetchData from './data'
+
+
 
 dom.search.addEventListener('click', ()=>{
+  
   fetchData()
+   dom.city.value = ""
 })
 
-dom.city.addEventListener("keyup", (event)=>{
+dom.city.addEventListener("keydown", (event)=>{
   if(event.key === "Enter")
   fetchData()
 })
+
+
 fetchData()
