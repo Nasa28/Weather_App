d9a309a6ffbaa27390ce940624eb724f3db7e7f2
Nasa28 <kaluchinasaagu@gmail.com>
2021-08-24 01:10:07 +0100

    fix linters

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 import * as dom from './dom';
 
-const fetchData = async () => {
+let fetchData = async() => {
   const apikey = 'bc92c530991c288d75d5cf2407507597';
   const location = dom.city.value || 'enugu';
   const url = `
@@ -40,10 +40,10 @@ const fetchData = async () => {
     });
   } catch (error) {
     if (error) {
-      dom.errors.innerHTML = `Enter a valid City name
-       `;
+      dom.errors.innerHTML = 'Enter a valid City name';
       setTimeout(location.reload(true), 2000);
     }
   }
 };
+
 export default fetchData();
